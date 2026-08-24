641cbb0448860c7465e63f0f1f04133f8ee91a30
Justin Rittenhouse <justin@jrcomputing.net>
2026-08-24 08:38:49 -0400

    chore: finalize integration constants, manifest metadata, and lockfile

---
+++ custom_components/myride/const.py
@@ -0,0 +1,11 @@
+"""Constants for the My Ride K-12 integration."""
+
+DOMAIN = "myride"
+
+CONF_DISTRICT_ID = "district_id"
+CONF_DISTRICTS = "districts"
+CONF_REFRESH_TOKEN = "refresh_token"
+
+# Polling intervals in seconds
+DEFAULT_POLL_INTERVAL_ACTIVE = 30
+DEFAULT_POLL_INTERVAL_PASSIVE = 900
